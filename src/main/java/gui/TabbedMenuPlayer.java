b7f42722a70ed79f8568fa3abc4900fffa1e2584
NoXiiM <130613329+NoXiiM@users.noreply.github.com>
2026-05-23 18:51:59 +0200

    gui

---
+++ src/main/java/gui/TabbedMenuPlayer.java
@@ -0,0 +1,22 @@
+package gui;
+
+import controller.WelcomeController;
+
+import javax.swing.*;
+
+public class TabbedMenuPlayer {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JPanel gamePanelPlayer;
+    private JButton pokerButton;
+
+    private JFrame frameChiamante;
+    private WelcomeController controller;
+
+    public TabbedMenuPlayer(JFrame mainframe, String username) {
+        this.frameChiamante = mainframe;
+
+
+    }
+
+}

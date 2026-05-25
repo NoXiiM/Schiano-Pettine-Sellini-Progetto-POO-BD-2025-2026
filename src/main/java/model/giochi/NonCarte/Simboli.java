f6a95efbdee6bdfb09cf6cfb0c00b40f972f736d
Mirko <mirkopettine06@gmail.com>
2026-05-25 18:35:26 +0200

    Cambio simboli in enumeration, cambio gestione percorsi con un HashMap<Simboli,String>; aggiunta Pop-up nel caso di credito insufficiente per la puntata; ridimensionamento della probabilità con l'aggiunto del simbolo zero e annullando il tornaconto di credito del singolo diamante

---
+++ src/main/java/model/giochi/NonCarte/Simboli.java
@@ -0,0 +1,5 @@
+package model.giochi.NonCarte;
+
+public enum Simboli {
+    diamante,ciliegia,cocomero,sette,triplodiamante,zero
+}
--- src/main/java/model/giochi/NonCarte/SlotMachine.java
+++ src/main/java/model/giochi/NonCarte/SlotMachine.java
@@ -1,75 +1,79 @@
 package model.giochi.NonCarte;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import static model.giochi.NonCarte.Simboli.*;
 
 public class SlotMachine {
-    private ArrayList<String> simboli;
-    private ArrayList<String> simboliPath;
+
+    private HashMap<Simboli, String> collegamentoSimboli;
+    private ArrayList<Simboli> simboli;
     private int numeroSimboli;
 
     public SlotMachine(){
         simboli = new ArrayList<>();
-        simboliPath = new ArrayList<>();
+        collegamentoSimboli =  new HashMap<>();
 
-        simboli.add("Diamante");
-        simboli.add("Ciliegia");
-        simboli.add("Cocomero");
-        simboli.add("Sette");
-        simboli.add("TriploDiamante");
+            simboli.add(diamante);
+            simboli.add(ciliegia);
+            simboli.add(cocomero);
+            simboli.add(sette);
+            simboli.add(triplodiamante);
+            simboli.add(zero);
 
-        simboliPath.add("/IconeSlotmachin/DiamanteIcon.png");
-        simboliPath.add("/IconeSlotmachin/CiliegiaIcon.png");
-        simboliPath.add("/IconeSlotmachin/CocomeroIcon.png");
-        simboliPath.add("/IconeSlotmachin/setteIcon.png");
-        simboliPath.add("/IconeSlotmachin/TriploDiamanteIcon.png");
+
+        collegamentoSimboli.put(diamante,"/IconeSlotmachin/DiamanteIcon.png");
+        collegamentoSimboli.put(ciliegia,"/IconeSlotmachin/CiliegiaIcon.png");
+        collegamentoSimboli.put(cocomero,"/IconeSlotmachin/CocomeroIcon.png");
+        collegamentoSimboli.put(sette,"/IconeSlotmachin/setteIcon.png");
+        collegamentoSimboli.put(triplodiamante,"/IconeSlotmachin/TriploDiamanteIcon.png");
+        collegamentoSimboli.put(zero, "/IconeSlotmachin/ZeroIcon.png");
+
         numeroSimboli = simboli.size();
 
     }
-    public String getSimboloCasuale(){
-        return (simboli.get((int)(Math.random()%(numeroSimboli))));
+    public Simboli getSimboloCasuale(){
+        Random random = new Random();
+        return (simboli.get(random.nextInt(numeroSimboli)));
     }
-    public float getsaldopartita(String s1, String s2, String s3, float saldo){
+    public float getsaldopartita(Simboli s1, Simboli s2, Simboli s3, float saldo){
         float creditoUscente = 0;
-        if(s1.equals(s2) && s2.equals(s3)){// la proprietà transitiva dell'uguale garantisce in questo modo cche siano tutti e 3 uguali
-            if(s1.equals(simboli.get(numeroSimboli-1))){// Ultimo simbolo di cui index corrisponde la grandezza, il triplodiamante
+        if(s1==s2 && s2==s3){// la proprietà transitiva dell'uguale garantisce in questo modo cche siano tutti e 3 uguali
+            if(s1==triplodiamante){// Ultimo simbolo di cui index corrisponde la grandezza, il triplodiamante
                 creditoUscente = saldo * 10;
             }
-            if(s1.equals(simboli.get(numeroSimboli-2))){// posizione sette
+            if(s1==sette){// posizione sette
                 creditoUscente = saldo * 7;
             }
-            if(s1.equals(simboli.get(numeroSimboli-3))){// posizione cocomero
+            if(s1==cocomero){// posizione cocomero
                 creditoUscente = saldo * 5;
             }
-            if(s1.equals(simboli.get(numeroSimboli-4))){// posizione ciliegia
+            if(s1==ciliegia){// posizione ciliegia
                 creditoUscente = saldo * 3;
             }
-            if(s1.equals(simboli.get(numeroSimboli-5))){// posizione diamante
+            if(s1==diamante){// posizione diamante
                 creditoUscente = saldo * 8;
             }
         }
         // Il prossimo controllo permette di capire se uno dei 3 simboli è un diamante
-        if(s1.equals(simboli.get(numeroSimboli-5)) || s2.equals(simboli.get(numeroSimboli-5)) || s3.equals(simboli.get(numeroSimboli-5))) {
+        if(s1==diamante || s2==diamante || s3==diamante) {
             // il prossimo controllo è vero solo se tutti i simboli sono diversi ma almeno uno è un diamante
-            if(!s1.equalsIgnoreCase(s2) && !s2.equalsIgnoreCase(s3) && !s1.equalsIgnoreCase(s3)) {
+            if(!(s1==s2) && !(s2==s3) && !(s1==s3)) {
 
-                creditoUscente = saldo * 2;
+                //creditoUscente = saldo * 1; rimosso dato che la probabilità di vincita diverrebbe del 40%
             }
             else {// qui ci si finesce se solo 2 simboli sono diamante
-                creditoUscente = saldo * 4;
+                creditoUscente = saldo * 2;
             }
         }
         return creditoUscente;
     }
 
-    public ArrayList<String> getSimboli() {
-        return simboli;
+
+    public HashMap<Simboli,String> getCollegamentoSimboli() {
+        return collegamentoSimboli;
     }
 
-    public ArrayList<String> getSimboliPath() {
-        return simboliPath;
-    }
-
-    public int getNumeroSimboli() {
-        return numeroSimboli;
-    }
 }

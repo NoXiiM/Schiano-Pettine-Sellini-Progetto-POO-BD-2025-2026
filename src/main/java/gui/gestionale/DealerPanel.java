5b4c901e19d8ed82a309b1e9a9c127941c04386a
Tigor <sellinimatteo4@gmail.com>
2026-07-27 16:31:32 +0200

    collegamento DealerPanel

---
+++ src/main/java/gui/gestionale/DealerPanel.java
@@ -0,0 +1,39 @@
+package gui.gestionale;
+
+import controller.gestionale.DipendenteWelcomeController;
+
+import javax.swing.*;
+
+public class DealerPanel {
+    private JPanel dealer;
+    private JTabbedPane dealerPanel;
+    private JTextArea textAreaSessioni;
+    private JList listaSessioni;
+    private JButton cercaButton;
+    private JButton logoutButton;
+    private JButton aggiornaListaButton;
+    private JTextField textField1;
+    private JButton attivaSospettoButton;
+    private JPanel gestioneAccountPanel;
+    private JButton cambiaPasswordButton;
+    private JButton cambiaUsernameButton;
+    private JButton resettaPasswordButton;
+    private JLabel usernameSessionePanel;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+
+    private DipendenteWelcomeController controller;
+    private JFrame frameChiamante;
+
+    public DealerPanel(DipendenteWelcomeController controller, JFrame frameChiamante)
+    {
+        JFrame frameChiamato = new JFrame("dealerPanel");
+        frameChiamato.setContentPane(dealer);
+        frameChiamato.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameChiamato.pack();
+        frameChiamato.setVisible(true);
+
+        this.controller = controller;
+        this.frameChiamante = frameChiamante;
+    }
+}
